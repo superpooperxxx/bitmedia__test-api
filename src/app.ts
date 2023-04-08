c08b70aee40517f0f25875a0c8f48d97c6f53502
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-04-08 12:41:27 +0300

    added routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express, { Application } from 'express';
+import pokemonsRouter from './routes/pokemonsRoutes';
+import userPokemonsRouter from './routes/userPokemonsRoutes';
 
 const app: Application = express();
+
+app.use('/api/pokemons', pokemonsRouter);
+app.use('/api/user-pokemons', userPokemonsRouter);
 
 export default app;
